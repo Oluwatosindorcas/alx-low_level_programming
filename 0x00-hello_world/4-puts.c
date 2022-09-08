be3e0808b8a206d243cdf586d1d957e788d59c68
Oluwatosindorcas <oluwatosindorcas68@gmail.com>
2022-09-08 15:32:40 -0700

    putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,20 @@
-
 #include <stdio.h>
-
-
 
 /**
  *
- * * main - print the string in the put function
+ *  * main - Entry point
  *
- * *
+ *   *
  *
- * * Description: using the main function
+ *    * Return: always 0
  *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ *     */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
